60b222faa8c0e885109905203ed17f501837bfad
jorgealvarezfidalgo <uo258524@uniovi.es>
2019-02-20 22:52:50 +0100

    Basic prototype interface created.
    Includes a home page, a popup for the login, a script for the home page functionality as well as a cascade stylesheet.

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,10 @@
+
+const auth = require('solid-auth-client');
+
+$('.login-btn').click(() => {
+  auth.popupLogin({ popupUri: 'popup.html' });
+});
+
+$('#logout-btn').click(() => {
+  auth.logout();
+});
