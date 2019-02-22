6828813b8ada126284e653242a5d90a35175592c
jorgealvarezfidalgo <uo258524@uniovi.es>
2019-02-22 18:58:30 +0100

    Introduced 'Join' menu, 'Cancel' functionality

--- src/main/index.js
+++ src/main/index.js
@@ -8,10 +8,12 @@ const DataSync = require('../lib/datasync');
 
 let core = new Core(auth.fetch);
 let userWebId;
-let interlocutorWebId;
+let interlocWebId;
 let refreshIntervalId;
 let dataSync = new DataSync(auth.fetch);
 let userDataUrl;
+let chatsToJoin = [];
+let semanticChat;
 
 $('.login-btn').click(() => {
   auth.popupLogin({ popupUri: 'popup.html' });
@@ -80,7 +82,7 @@ $('#start-new-chat-btn').click(async () => {
 
   if (await core.writePermission(dataUrl, dataSync)) {
     $('#new-chat-options').addClass('hidden');
-    interlocutorWebId = $('#contacts').val();
+    interlocWebId = $('#contacts').val();
     userDataUrl = dataUrl;
     setUpNewConversation();
   } else {
@@ -92,3 +94,117 @@ $('#start-new-chat-btn').click(async () => {
 async function setUpNewConversation() {
   //Initialize conversation
 }
+
+$('#join-btn').click(async () => {
+  if (userWebId) {
+    afterChatOption();
+    $('#join-chat-options').removeClass('hidden');
+    $('#join-data-url').prop('value', core.getDefaultDataUrl(userWebId));
+    $('#join-looking').addClass('hidden');
+
+    if (chatsToJoin.length > 0) {
+      $('#join-loading').addClass('hidden');
+      $('#join-form').removeClass('hidden');
+      const $select = $('#chat-urls');
+      $select.empty();
+
+      chatsToJoin.forEach(game => {
+        let name = chat.name;
+
+        if (!name) {
+          name = chat.chatUrl;
+        }
+
+        $select.append($(`<option value="${game.gameUrl}">${name} (${game.opponentsName})</option>`));
+      });
+    } else {
+      $('#no-join').removeClass('hidden');
+    }
+  } else {
+    $('#login-required').modal('show');
+  }
+});
+
+$('#join-chat-btn').click(async () => {
+  if ($('#join-data-url').val() !== userWebId) {
+    userDataUrl = $('#join-data-url').val();
+
+    if (await core.writePermission(userDataUrl, dataSync)){
+      $('#join-chat-options').addClass('hidden');
+      const chatUrl = $('#chat-urls').val();
+
+      let i = 0;
+
+      while (i < chatsToJoin.length && chatsToJoin[i].chatUrl !== chatUrl) {
+        i++;
+      }
+
+      const chat = chatsToJoin[i];
+
+      // remove it from the array so it's no longer shown in the UI
+      chatsToJoin.splice(i, 1);
+
+      // setUpForEveryChatOption();
+      // interlocWebId = chat.interlocutorWebId;
+      // semanticChat = await core.joinExistingChat(chatUrl, chat.invitationUrl, interlocWebId, userWebId, userDataUrl, dataSync, chat.fileUrl);
+
+        // webrtc = new WebRTC({
+          // userWebId,
+          // userInboxUrl: await core.getInboxUrl(userWebId),
+          // interlocutorWebId: interlocWebId,
+          // interlocutorWebId: await core.getInboxUrl(interlocWebId),
+          // fetch: auth.fetch,
+          // initiator: false,
+          // onNewData: rdfjsSource => {
+            // let newMessageFound = false;
+
+            // core.checkForNewMessage(semanticChat, dataSync, userDataUrl, rdfjsSource, (san, url) => {
+              // semanticChat.loadMessage(san, {url});
+              // //semanticChat.getChat().fen()
+              // updateStatus();
+              // newMessageFound = true;
+            // });
+
+            // if (!newMessageFound) {
+              // core.checkForGiveUpOfChat(semanticChat, rdfjsSource, (agentUrl, objectUrl) => {
+                // semanticChat.loadGiveUpBy(agentUrl);
+                // $('#interlocutor-quit').modal('show');
+              // });
+            // }
+          // },
+          // onCompletion: () => {
+            // $('#real-time-setup').modal('hide');
+          // },
+          // onClosed: (closedByUser) => {
+            // if (!closedByUser && !$('#interlocutor-quit').is(':visible')) {
+              // $('#interlocutor-quit').modal('show');
+            // }
+          // }
+        // });
+
+        // webrtc.start();
+
+        // //$('#real-time-setup .modal-body ul').append('<li>Response sent</li><li>Setting up direct connection</li>');
+        // //$('#real-time-setup').modal('show');
+      
+
+      // setUpWindow(semanticChat);
+      // setUpAfterEveryChatOptionIsSetUp();
+    } else {
+      $('#write-permission-url').text(userDataUrl);
+      $('#write-permission').modal('show');
+    }
+  } else {
+    console.warn('We are pretty sure you do not want to remove your WebID.');
+  }
+});
+
+$('.btn-cancel').click(() => {
+  interlocWebId = null;
+
+  $('#chat').addClass('hidden');
+  $('#new-chat-options').addClass('hidden');
+  $('#join-chat-options').addClass('hidden');
+  $('#continue-chat-options').addClass('hidden');
+  $('#chat-options').removeClass('hidden');
+});
