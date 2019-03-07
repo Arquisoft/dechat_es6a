a2d00788e42d397566b8350b165467fd49a04b2a
cristianladoglz <cris.1998@hotmail.es>
2019-03-07 16:17:01 +0100

    Added one test case more

--- src/test/e2e-tests/specs/interaction.js
+++ src/test/e2e-tests/specs/interaction.js
@@ -1,10 +1,16 @@
 'use strict'
 
-describe( 'NetChat Interactions', function () {});
+describe( 'dechat interactions', function () {});
 var path = require( 'C:\Program Files\nodejs\' );
 
 browser.get( '/' );
 
 it ( 'should redirect to /', function () {
     expect( browser.getLocationAbsUrl() ).toMatch( '/' );
+});
+
+it ( 'should click login', function () {
+    element( by.id( 'new-btn' ) ).click().then( function () {
+            expect( element( by.id( 'data-url' ) ).count() ).toBe( 1 );
+    });
 });
