1a82752ec593ce8a32d409d15c37d9f372daae9e
cristianladoglz <cris.1998@hotmail.es>
2019-03-07 17:26:33 +0100

    Update test cases

--- src/test/e2e-tests/specs/interaction.js
+++ src/test/e2e-tests/specs/interaction.js
@@ -1,16 +1,19 @@
 'use strict'
 
-describe( 'dechat interactions', function () {});
 var path = require( 'C:\Program Files\nodejs\' );
 
 browser.get( '/' );
 
-it ( 'should redirect to /', function () {
-    expect( browser.getLocationAbsUrl() ).toMatch( '/' );
+describe( 'dechat interactions', function () {
+    it ( 'should redirect to /', function () {
+        expect( browser.getLocationAbsUrl() ).toMatch( '/' );
+    });
 });
 
-it ( 'should click login', function () {
-    element( by.id( 'new-btn' ) ).click().then( function () {
-            expect( element( by.id( 'data-url' ) ).count() ).toBe( 1 );
+describe( 'dechat interactions', function () {
+    it ( 'should click login', function () {
+        element( by.id( 'new-btn' ) ).click().then( function () {
+                expect( element( by.id( 'data-url' ) ).count() ).toBe( 1 );
+        });
     });
 });
