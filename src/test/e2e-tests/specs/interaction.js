23b6c53583e657736103831da85873670f56ebdd
AlvaroGonzalezC <36399917+AlvaroGonzalezC@users.noreply.github.com>
2019-03-08 21:18:51 +0100

    New tests for the initial panel

--- src/test/e2e-tests/specs/interaction.js
+++ src/test/e2e-tests/specs/interaction.js
@@ -20,6 +20,23 @@ describe( 'dechat interactions', function () {
     it ( 'Check login button text ', function () {
             var btnLogin = element(by.id('nav-login-btn'));
             expect(btnLogin.getText()).toEqual('Log in');
+    });
+});
+
+describe( 'dechat interactions', function () {
+    it ( 'Check that the new Chat button is hidden when you press it', function () {
+         element( by.id( 'new-btn' ) ).click().then( function () {
+                expect( element( by.id( 'new-btn' ).isDisplayed()).toBe( true );
+        });
+    });
+    it ( 'Check that the join Chat button is hidden when you press it ', function () {
+         element( by.id( 'join-btn' ) ).click().then( function () {
+                expect( element( by.id( 'join-btn' ).isDisplayed()).toBe( true );
+        });
+    });
+    it ( 'Check that the continue Chat button is hidden when you press it ', function () {
+         element( by.id( 'open-btn' ) ).click().then( function () {
+                expect( element( by.id( 'open-btn' ).isDisplayed()).toBe( true );
         });
     });
 });
