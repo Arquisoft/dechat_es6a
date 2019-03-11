fb0389c46ee532e5da3dcea587f156af4a00cb68
Othmane Bakhtaoui <UO259323@uniovi.es>
2019-03-11 19:55:16 +0100

    Merge with Chat-fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis!\n'); // build should pass now!
+  res.end('Hello Travis!\n');
 }).listen(8080, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8080/');
 
